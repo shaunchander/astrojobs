664a57effe5b4b4d4524e619ac803e6ee1efd9b2
shaunchander <shaunchander@gmail.com>
2023-04-14 09:46:46 -0500

    test

--- apps/frontend/astro.config.ts
+++ apps/frontend/astro.config.ts
@@ -26,7 +26,8 @@ export default defineConfig({
 		svelte()
 	],
 	output: "server",
-	adapter: cloudflare({
-		mode: "directory"
-	})
+	adapter: cloudflare(),
+	vite: {
+		envDir: "./../../"
+	}
 })
