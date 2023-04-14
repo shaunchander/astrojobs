562b2d2882027bd665705e6ae95056680a691b3f
shaunchander <shaunchander@gmail.com>
2023-04-14 10:59:36 -0500

    change

---
+++ apps/frontend/src/pages/server/generate-stripe-checkout.ts
@@ -0,0 +1,34 @@
+import type { APIRoute } from "astro"
+import Stripe from "stripe"
+import { seoConfig } from "@lib/seoConfig"
+
+const stripe = new Stripe(import.meta.env.STRIPE_KEY, {
+	apiVersion: "2022-11-15"
+})
+
+export const get: APIRoute = async () => {
+	try {
+		const session = await stripe.checkout.sessions.create({
+			line_items: [
+				{
+					price: "price_1Mv9yrHOTwAqSFb2L7Vq4uQT",
+					quantity: 1
+				}
+			],
+			mode: "payment",
+			success_url: `${seoConfig.baseURL}/offer-posted`,
+			cancel_url: `${seoConfig.baseURL}/post-an-offer`
+		})
+
+		return {
+			status: 200,
+			body: JSON.stringify({ url: session.url, id: session.id })
+		}
+	} catch (err) {
+		console.error(err)
+		return {
+			status: 400,
+			body: "Bad request."
+		}
+	}
+}
---
+++ apps/frontend/src/pages/server/send-email.ts
@@ -0,0 +1,46 @@
+import Mailgun from "mailgun.js"
+import FormData from "form-data"
+import type { APIRoute } from "astro"
+
+const mailgun = new Mailgun(FormData)
+const mg = mailgun.client({ username: "api", key: import.meta.env.MAILGUN_KEY })
+export const post: APIRoute = async (ctx) => {
+	let body
+	try {
+		body = await ctx.request.json()
+	} catch (err) {
+		console.error(err)
+		return {
+			status: 400,
+			body: "Bad request."
+		}
+	}
+
+	if (!body._id) {
+		return {
+			status: 400,
+			body: "Bad request."
+		}
+	}
+	try {
+		console.log("Sending email to", body.email, "...")
+		mg.messages.create("mg.astrojobs.net", {
+			to: body.email,
+			from: "Astro Jobs <support@astrojobs.net>",
+			subject: "Astro Jobs - Your job offer has been posted!",
+			template: "post-success",
+			"v:_id": body._id
+		})
+		console.log("Email sent.")
+		return {
+			status: 200,
+			body: "OK."
+		}
+	} catch (err) {
+		console.error(err)
+		return {
+			status: 400,
+			body: "Bad request."
+		}
+	}
+}
