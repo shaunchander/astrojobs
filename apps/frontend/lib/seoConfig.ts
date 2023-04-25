9e8be5ff1be071879a72dfb6ec8c33003577eb6b
shaunchander <shaunchander@gmail.com>
2023-04-25 13:16:57 -0500

    🎉 update seo and launch

--- apps/frontend/lib/seoConfig.ts
+++ apps/frontend/lib/seoConfig.ts
@@ -8,8 +8,8 @@ export const seoConfig = {
 		"Find jobs or post offers centered around Astro, the framework that makes the web faster.", // Change this to be your website's description.
 	type: "website",
 	image: {
-		url: "https://picsum.photos/1200/630", // Change this to your website's thumbnail.
-		alt: "OpenGraph thumbnail description.", // Change this to your website's thumbnail description.
+		url: "https://astrojobs.net/thumbnail.png", // Change this to your website's thumbnail.
+		alt: "AstroJobs", // Change this to your website's thumbnail description.
 		width: 1200,
 		height: 630
 	},
